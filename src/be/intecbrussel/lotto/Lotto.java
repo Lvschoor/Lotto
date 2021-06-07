74b1fbc0a8fff0849fb98653a9e6d7c34a4162db
Lvschoor <lvschoor@gmail.com>
2021-06-07 20:05:37 +0200

    Corrected bug that allowed for identical values

--- src/be/intecbrussel/lotto/Lotto.java
+++ src/be/intecbrussel/lotto/Lotto.java
@@ -8,33 +8,42 @@ public final class Lotto {
     private static Random generate = new Random();
     private static int[] draw = new int[7];
     private static int[] firstSixNumbers = new int[6];
+    private static boolean alreadyExists = true;
+    private static int temp;
 
-//generate 7 different numbers (1-45) and store in array draw
+
+    //generate 7 different numbers (1-45) and store in array draw
     private static void generateLottoResults() {
         for (int i = 0; i < 7; i++) {
-            boolean alreadyExists = true;
-            int temp = generate.nextInt(45) + 1;
-            while (alreadyExists) {
-                for (int j : draw) {
-                    if (temp == j) {
-                        temp = generate.nextInt(45) + 1;
-                    } else {
-                        alreadyExists = false;
-                    }
-                }
-            }
+
+            do {
+                temp = generate.nextInt(45) + 1;
+            } while (alreadyExistsInArray(temp));
+
             draw[i] = temp;
         }
-
     }
-// take the first six numbers, store in firstSixNumbers and sort them
+
+    // test to see if number already is in the array
+    private static boolean alreadyExistsInArray(int testNumber) {
+
+        for (int j : draw) {
+            if (testNumber == j) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // take the first six numbers, store in firstSixNumbers and sort them
     private static void firstSix() {
         for (int i = 0; i < 6; i++) {
             firstSixNumbers[i] = draw[i];
         }
         Arrays.sort(firstSixNumbers);
     }
-// print the result of the Lotto draw; reserve number is the 7th in the draw
+
+    // print the result of the Lotto draw; reserve number is the 7th in the draw
     public static void printResults() {
         generateLottoResults();
         firstSix();
